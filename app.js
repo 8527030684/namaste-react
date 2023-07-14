c23613f88f100dcd65a680d7dddd120b75a58e2f
saurabh <shakyasaurabh8527@gmail.com>
2023-07-14 09:17:01 +0530

    laying foundation

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-const heading = React.createElement("h1", {class: "heading"}, "Hello world from react");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const TitleComponent = () => {
+    return (
+        <div className="container">
+            <h1>TitleComponent component</h1>
+        </div>
+    )
+};
+
+const HeadingComponent = () => {
+    return (
+        <div className="container">
+            <TitleComponent />
+            <h1>Namaste react functional component</h1>
+        </div>
+    )
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent />);
