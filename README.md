2e439607756ee3ef8c8f0af9810aa79caa1d9a26
saurabh <shakyasaurabh8527@gmail.com>
2023-08-27 21:56:30 +0530

    worked on hooks useState

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Namaste React
+
+# React Hooks
+(Normal JS utility functions)
+- useState() - superpowerful State Variable in react
+- useEffect()
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,25 @@
-
+import { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import resList from '../utils/mockData';
 
 const Body = () => {
+    const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
     return (
         <div className="body-container">
-            <div className="search">Search</div>
+            <div className="search">
+                <button 
+                    className="filter-btn" 
+                    onClick={() => {
+                        const filteredList = listOfRestaurants.filter(res => res.data.avgRating > 4);
+                        setListOfRestaurants(filteredList);
+                    }}>
+                    Top Rated Restaurants
+                </button>
+            </div>
             <div className="res-conatainer">
                 {
-                    resList.map((restaurant) => {
+                    listOfRestaurants.map((restaurant) => {
                         return <RestaurantCard key={restaurant.data.id} resData={restaurant} />
                     })
                 }
