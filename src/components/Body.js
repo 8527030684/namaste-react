9ee07416605c5e5d6034ce51835def8a2b0d3434
saurabh <shakyasaurabh8527@gmail.com>
2023-09-05 23:56:50 +0530

    working on file on card go to restaurant list

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import Shimmer from "./Shimmer";
+import { Link } from "react-router-dom";
 // import resList from '../utils/mockData';
 
 const Body = () => {
@@ -21,11 +22,11 @@ const Body = () => {
 
         const json = await data.json();
         console.log(json);
-        setListOfRestaurants(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-        setFilteredRestaurant(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setFilteredRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     };
 
-    return listOfRestaurants.length === 0 ? (
+    return listOfRestaurants?.length === 0 ? (
         <Shimmer />
     ) :  (
         <div className="body-container">
@@ -56,7 +57,9 @@ const Body = () => {
             <div className="res-conatainer">
                 {
                     filteredRestaurant.map((restaurant) => {
-                        return <RestaurantCard  key={restaurant?.info.id} resData={restaurant} />
+                        return <Link key={restaurant?.info.id} to={"/restaurants/" + restaurant?.info.id}>
+                                    <RestaurantCard resData={restaurant} />
+                                </Link>
                     })
                 }
             </div>
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,5 @@
 export const CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const LOGO_URL = "https://www.shutterstock.com/shutterstock/photos/1934009081/display_1500/stock-vector-order-food-logo-design-illustration-1934009081.jpg";
+
+export const MENU_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.4826264&lng=77.2986963&restaurantId=";
